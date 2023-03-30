bb98ee7f9aad9fb8d281a5c1a8a101cb623e273a
leshliao <leviliao38@gmail.com>
2023-03-31 02:22:09 +0800

    Add python coding conventions

---
+++ coding_Interview/coding_conventions/conventions_example.py
@@ -0,0 +1,18 @@
+import time
+import os
+
+# This is a simple comment.
+
+PI = 3.14159
+PORT_NUMBER = 8080
+
+
+class MyClass:
+
+    def my_function(self):
+        pass
+
+    def function_2(self, x, y):
+        list = [1, 2, 3, 4]
+        a = a + x
+        return a
