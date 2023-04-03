05f63380c2e5605909e4127594e8d96b10fe89e5
leshliao <leviliao38@gmail.com>
2023-04-04 00:54:28 +0800

    Add more example

--- coding_Interview/oo/example.py
+++ coding_Interview/oo/example.py
@@ -1,5 +1,26 @@
 # Transport can be anything. Horse, boat, airplane
+"""
+Note: It's different with Java programming.
 
+1. subclass constructor __init__ will override super class constructor __init__
+    which means instantiate a subclass that won't call super class __init__
+    unless subclass have no constructor.
+2. Ducking type is similar to interface of Java.
+
+3. The private attributes using double underscore as the prefix
+
+4. static variable is different with instance variable
+    once you change class variable by instance, it turn into individual variable.
+    Ex:
+        Vehicle.vehicle_num = 123 # change Class variable and instance variable
+        # If change value of instance
+         car1 = Car('Tesla')
+         car1.vehicle_num = 456  # this variable is not same with Class variable
+    Conclusion:
+        If you wanna using static variable.
+        Just use class variable:
+            Vehicle.vehicle_num = 123
+"""
 from abc import ABC, abstractmethod
 
 
@@ -15,8 +36,10 @@ class Transport(ABC):
 
 class Vehicle(Transport):
     vehicle_num = 0
+    __my_private_variable = 'vehicle private data'
 
     def __init__(self):
+        print('Vehicle__init__')
         Vehicle.vehicle_num += 1
 
     def move(self):
@@ -32,6 +55,7 @@ class Vehicle(Transport):
 
 class Car(Vehicle):
     def __init__(self, model, color='white'):
+        super().__init__()
         self.model = model
         self.color = color
         pass
@@ -83,24 +107,38 @@ class Horse(Transport):
 
 if __name__ == "__main__":
 
+    # tr = Transport() # Can't instantiate abstract class
+
+    # instantiate superclass
     veh = Vehicle()
     veh.move()
     veh.stop()
+    print('Total vehicle:' + str(Vehicle.vehicle_num))
 
+    # instantiate subclass
     car1 = Car('Tesla')
     car1.move()
     car1.stop()
+    print('Total vehicle:' + str(Vehicle.vehicle_num))
 
+    # instantiate subclass
     car2 = Car('Tesla', color='red')
     car2.move()
     car2.stop()
-    print('Total vehicle:' + str(Vehicle.get_vehicle_num()))
+    print('Total vehicle:' + str(Vehicle.vehicle_num))
+
+    # we denote private attributes using double underscore as the prefix
+    # print('Get private value:' + str(car2.__my_private_variable)) # can not get private variable
+
+    # using staticmethod
     print('100 km/h = ' + str(Car.convert_kmh_to_mph(100)) + ' mph')
 
     plane = Airplane()
     plane.move()
     plane.stop()
-    print('Total vehicle:' + str(Vehicle.get_vehicle_num()))
+
+    print('Total vehicle:' + str(Vehicle.vehicle_num))
+    print('Total vehicle(instance):' + str(plane.vehicle_num))
 
     horse = Horse()
     horse.move()
