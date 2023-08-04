2c8108f6669ee7ab9c7f2895011e406bc85af7d9
leshliao <leviliao38@gmail.com>
2023-08-05 01:07:54 +0800

    Add leetcode solution: Valid Anagram

---
+++ leetcode/TopInterviewQuestions/Strings/04 Valid Anagram/Solution.py
@@ -0,0 +1,42 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        s_dictionary = self.calculate_letter(s)
+        t_dictionary = self.calculate_letter(t)
+
+        for key in s_dictionary:
+            # print(key, '->', s_dictionary.get(key))
+            # print(key, '->', t_dictionary.get(key))
+            if s_dictionary.get(key) != t_dictionary.get(key):
+                return False
+
+        for key in t_dictionary:
+            # print(key, '->', s_dictionary.get(key))
+            # print(key, '->', t_dictionary.get(key))
+            if s_dictionary.get(key) != t_dictionary.get(key):
+                return False
+
+        return True
+
+    def calculate_letter(self, temp: str) -> dict:
+
+        my_dictionary = {}
+        for element in temp:
+            val = my_dictionary.get(element)
+            if val != None:
+                sum = int(val) + 1
+                my_dictionary.update({element: str(sum)})
+            else:
+                my_dictionary.update({element: "1"})
+
+        return my_dictionary
+
+
+s = Solution()
+ans = s.isAnagram("anagram", "nagaram")
+print("ans:"+str(ans))
+print("expect: True")
+
+ans = s.isAnagram("rat", "car")
+print("ans:"+str(ans))
+print("expect: False")
