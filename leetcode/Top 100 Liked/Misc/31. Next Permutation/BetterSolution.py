25c601261750db2171ef09654e115241fe3298e7
leshliao <leviliao38@gmail.com>
2023-10-22 16:36:49 -0700

    add leetcode solution: 31. Next Permutation

---
+++ leetcode/Top 100 Liked/Misc/31. Next Permutation/BetterSolution.py
@@ -0,0 +1,64 @@
+import unittest
+from typing import List
+
+class Solution:
+    '''
+        Refer to: Oloy, Link:https://leetcode.com/problems/next-permutation/discuss/1909728/Simple-9-line-Python-Solution-with-Detailed-Explanation-(Easy-Understand-for-Beginners!)
+    '''
+    def nextPermutation(self, nums: List[int]) -> None:
+        for i in range(len(nums)-1, 0, -1):
+            # find the index of the last peak
+            if nums[i - 1] < nums[i]:
+                nums[i:] = sorted(nums[i:])
+                # get the index before the last peak
+                j = i - 1
+                # swap the pre-last peak index with the value just large than it
+                for k in range(i, len(nums)):
+                    if nums[j] < nums[k]:
+                        nums[k], nums[j] = nums[j], nums[k]
+                        return nums
+
+        return nums.reverse()
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_ex1(self):
+        nums = [1,2,3]
+        excepted = [1,3,2]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_ex2(self):
+        nums = nums = [3,2,1]
+        excepted = [1,2,3]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_ex3(self):
+        nums = [1,1,5]
+        excepted = [1,5,1]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_submit_1(self):
+        nums = [2,3,1]
+        excepted = [3,1,2]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_submit_1(self):
+        nums = [1,3,2]
+        excepted = [2,1,3]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_private_1(self):
+        nums = [1,2,4,3]
+        excepted = [1,3,2,4]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ leetcode/Top 100 Liked/Misc/31. Next Permutation/Solution.py
@@ -0,0 +1,74 @@
+import unittest
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+
+        nums_len = len(nums)
+        for i in range(nums_len-1, 0, -1):
+            if nums[i] > nums[i-1]:
+                if i == nums_len-1:
+                    self.swap_num(nums, i, i-1)
+                    break
+                else:
+                    for j in range(nums_len-1, i-1, -1):
+                        if nums[j] > nums[i-1]:
+                            self.swap_num(nums, i-1, j)
+                            break
+                    self.sort_sub_array(nums, i, nums_len)
+                    break
+            if i == 1:
+                self.sort_sub_array(nums, 0, nums_len)
+
+    def swap_num(self,nums, x, y):
+        temp = nums[x]
+        nums[x] = nums[y]
+        nums[y] = temp
+
+    def sort_sub_array(self,nums, begin, end):
+        sub_list = sorted(nums[begin:end])
+        for i in range(begin,end):
+            nums[i] = sub_list[i-begin]
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_ex1(self):
+        nums = [1,2,3]
+        excepted = [1,3,2]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_ex2(self):
+        nums = nums = [3,2,1]
+        excepted = [1,2,3]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_ex3(self):
+        nums = [1,1,5]
+        excepted = [1,5,1]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_submit_1(self):
+        nums = [2,3,1]
+        excepted = [3,1,2]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_submit_1(self):
+        nums = [1,3,2]
+        excepted = [2,1,3]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+    def test_private_1(self):
+        nums = [1,2,4,3]
+        excepted = [1,3,2,4]
+        self.s.nextPermutation(nums)
+        self.assertEqual(nums, excepted)
+
+if __name__ == '__main__':
+    unittest.main()
