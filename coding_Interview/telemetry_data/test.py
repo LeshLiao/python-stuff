5b224309e8d7b71de412dafbf6d4079ef56eaf56
LeshLiao <dlnx38@gmail.com>
2026-03-12 08:55:47 +0800

    - Add more examples

--- coding_Interview/telemetry_data/test.py
+++ coding_Interview/telemetry_data/test.py
@@ -7,6 +7,14 @@ count = defaultdict(int)
 
 # filter、map、reduce
 
+'''
+ - Container	Example
+    List	[1, 2, 3]
+    Tuple	(1, 2, 3)
+    Set	{1, 2, 3}
+    Dictionary	{"a":1, "b":2}
+'''
+
 
 '''
 [
@@ -26,18 +34,46 @@ all_devices = [
   ("device2","ERROR")
 ]
 
-def show_all_device(devices):
+def count_error_devices(devices):
 
   for key, value in devices:
     if value == "ERROR":
       count[key] += 1
-    return dict(count)
+
+  return dict(count)
 
 expected = {
   "device1": 2,
   "device2": 1
 }
 
-result = show_all_device(all_devices)
+result = count_error_devices(all_devices)
 
 print(expected == result)
+
+
+
+transactions = [
+  ("store1", 100),
+  ("store2", 200),
+  ("store1", 50),
+  ("store3", 300)
+]
+
+
+def count_stores_amount():
+  amount = defaultdict(int)
+  for key, value in transactions:
+    amount[key] += value
+
+  return dict(amount)
+
+result2 = count_stores_amount()
+
+expected2 = {
+  'store1': 150,
+  'store2':200,
+  'store3':300
+}
+
+print(expected2 == result2)
